8145d097aa1dedad784cbaaa5043525b178db2c3
nsentemova <sentemovanatalya@yandex.ru>
2022-12-16 17:12:21 +0300

    routing done

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router";
+import { ScaleLoader } from "react-spinners";
+import router from "./router/router";
+import "src/scss/index.scss";
+
+const SPINNER_COLOR = "hsl(22, 65%, 57%)";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider
+      router={router}
+      fallbackElement={<ScaleLoader color={SPINNER_COLOR} />}
+    />
   </React.StrictMode>
 );
