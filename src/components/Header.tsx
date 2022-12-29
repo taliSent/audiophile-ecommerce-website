a4aafb89f6f48e68a19d0e62c09e0be408455b53
nsentemova <sentemovanatalya@yandex.ru>
2022-12-29 22:25:51 +0300

    navbar tablet fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import Navbar from "./Navbar";
 import { BrowserView, MobileView, TabletView } from "react-device-detect";
 import logo from "src/assets/shared/logo.svg";
 import "src/scss/components/_header.scss";
+import { AnimatePresence } from "framer-motion";
 
 const Header: FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -23,28 +24,20 @@ const Header: FC = () => {
       <MobileView>
         <div className='header'>
           <div className='header__content'>
-            <div>
-              <div
-                className='header__content__icon--hamburger'
-                onClick={toggleIsOpen}
-              />
-              <img
-                src={logo}
-                alt='open-navbar'
-                className='header__content__logo'
-              />
-            </div>
-            {isMenuOpen ? (
-              <>
-                {/* <Navbar variant='header' />
-                <div className='header__content__mobile-menu'></div> */}
-              </>
-            ) : (
-              <></>
-            )}
-
+            <div
+              className='header__content__icon--hamburger'
+              onClick={toggleIsOpen}
+            />
+            <img
+              src={logo}
+              alt='open-navbar'
+              className='header__content__logo'
+            />
             <div className='header__content__icon--cart' />
           </div>
+          <AnimatePresence>
+            {isMenuOpen && <Navbar variant='header' isMenuOpen />}
+          </AnimatePresence>
         </div>
       </MobileView>
     </>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,29 @@
 import { FC } from "react";
+import { isDesktop } from "react-device-detect";
 import { Link } from "react-router-dom";
 import logo from "src/assets/shared/logo.svg";
+import { motion } from "framer-motion";
 import "src/scss/components/_navbar.scss";
 
 type NavbarT = {
   variant: "header" | "footer";
+  isMenuOpen: boolean;
 };
 
-const Navbar: FC<NavbarT> = ({ variant }: NavbarT) => {
+const Navbar: FC<NavbarT> = ({ variant, isMenuOpen }: NavbarT) => {
   return (
     <div className='navbar'>
-      <img src={logo} alt='logo' />
-      <div className={`navbar__links navbar__links--${variant}`}>
+      {/* <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      /> */}
+      {isDesktop && <img src={logo} alt='logo' />}
+      <div
+        className={`navbar__links navbar__links--${variant}--${
+          isMenuOpen ? "open" : "closed"
+        }`}
+      >
         <Link to='/'>HOME</Link>
         <Link to='/headphones'>HEADPHONES</Link>
         <Link to='/speaker'>SPEAKERS</Link>
